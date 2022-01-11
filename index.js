c2aa572a2e55d0ba8cbc1915f19a69175d9c76a2
tylergubser <tylergubser2200@gmail.com>
2022-01-11 11:25:14 -0600

    commit tree 22fac57eba6fdd64f5324af494ab3cb2e885e994

---
+++ index.js
@@ -0,0 +1,39 @@
+function add() {
+    return (number + number);
+}
+function subtract() {
+    return (number - number);
+}
+function multiply() {
+    return (number * number);
+}
+function divide() {
+    return (number / number);
+}
+function add(a, b) {
+    return (a + b);
+}
+function subtract(a, b) {
+    return (a - b);
+}
+function multiply(a, b) {
+    return (a * b);
+}
+function divide(a, b) {
+    return (a / b);
+} 
+
+function increment(n) {
+ return (++n);
+}
+
+function decrement(n) {
+    return(--n);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+} 
+function preserveDecimal(n) {
+    return parseFloat(n, 10)
+}
